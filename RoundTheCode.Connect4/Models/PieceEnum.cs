91e8b894fe9055d6dae143417600a79449bc0035
Dave <davidgrace00@gmail.com>
2021-05-29 12:19:11 +0100

    The demo for Connect 4

---
+++ RoundTheCode.Connect4/Models/PieceEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoundTheCode.Connect4.Models
+{
+    public enum PieceEnum
+    {
+        Yellow,
+        Red
+    }
+}
---
+++ RoundTheCode.Connect4/Models/Space.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoundTheCode.Connect4.Models
+{
+    public class Space
+    {
+        public PieceEnum? Piece { get; protected set; }
+
+        public void SetPiece(PieceEnum piece)
+        {
+            Piece = piece;
+        }
+
+    }
+}
